9c4438f0395cd9be86d70aeba279b81cbed6a28f
sirogamichandayo <ssirogamicota@gmail.com>
2020-09-12 23:19:11 +0900

    secure

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,8 +24,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-        if(env('APP_ENV') == 'production') {
-            URL::forceScheme('https');
+        if ($this->app->environment() === 'production') 
+            \URL::forceScheme('https');
         }
-    }
 }
