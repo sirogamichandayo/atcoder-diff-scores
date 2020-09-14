3632f244a1e79d640d4d10f4f3eff39b9e589c7d
sirogamichandayo <ssirogamicota@gmail.com>
2020-09-14 20:00:19 +0900

    add csrf

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -7,7 +7,7 @@
     {{-- search bar --}}
     <nav class="border-bottom navbar navbar-expand-md navbar-light bg-light">
         <form class="form-inline" method="post">
-            {{ csrf_field() }}
+            @csrf
             <input class="form-control mr-sm-2" type="text" 
                    value="{{$raw_ids}}" placeholder="AtCoder ID, ..." 
                    aria-label="Search" name="raw_ids">
