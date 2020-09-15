aeb5e397031857838324f857179c7612f32797f2
sirogamichandayo <ssirogamicota@gmail.com>
2020-09-15 23:52:03 +0900

    a

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,7 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->string('user_id');
+            $table->string('user_id')->unique();
             $table->integer('diff_sum');
         });
     }
