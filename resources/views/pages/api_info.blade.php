57797ce19123c8798ef762f07db1abebe6a0ecc5
sirogamichandayo <ssirogamicota@gmail.com>
2020-09-20 01:10:49 +0900

    add api info

--- resources/views/pages/api_info.blade.php
+++ resources/views/pages/api_info.blade.php
@@ -18,14 +18,22 @@
 
     <h3>Info</h3>
     <hr size=1>
-    <h5>Diff Sum Per Date</h5>
-    <p>日付でソートされてます</p>
+    <h5>Diff Per Date</h5>
+    <p>ユーザがその日に説いたdiffの合計です。日付でソートされてます。逐一計算しているのでかなり遅いです。</p>
     <p><b>Interface</b></p>
     <pre><code>{{$request->getUriForPath('') . '/api/resources/diff_per_date?user_id={user_id}'}};</code></pre>
     <p><b>Example</b></p>
     <ul><li>
         <a href="{{$request->getUriForPath('') . '/api/resources/diff_per_date?user_id=sirogamichandayo'}}">
         {{$request->getUriForPath('') . '/api/resources/diff_per_date?user_id=sirogamichandayo'}}    
+        </a>    
+    </li></ul>
+
+    <h5>Diff sum</h5>
+    <p>ユーザ毎のdiffの合計です。</p>
+    <ul><li>
+        <a href="{{$request->getUriForPath('') . '/api/resources/diff_sum'}}">
+        {{$request->getUriForPath('') . '/api/resources/diff_sum'}}    
         </a>    
     </li></ul>
 @endsection
