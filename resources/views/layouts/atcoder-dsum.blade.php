24c7f292466fada030de6b3502bfc56e363c96be
sirogamichandayo <ssirogamicota@gmail.com>
2020-09-21 19:50:25 +0900

    delete file

--- resources/views/layouts/atcoder-dsum.blade.php
+++ resources/views/layouts/atcoder-dsum.blade.php
@@ -42,7 +42,7 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>    
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.bundle.min.js"></script>
     <script src="https://unpkg.com/chartjs-plugin-colorschemes"></script>
-
+    
     <style>
     code {
         font-family: Consolas,"courier new";
